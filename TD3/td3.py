7ca45b9976f378d6de34478018cf20bf7f71625a
Sleeperalex <alexfigaro2003@gmail.com>
2024-12-12 12:50:36 +0100

    update

--- TD3/td3.py
+++ TD3/td3.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 def pickands_estimator(df: pd.DataFrame,column: str, k_n: int):
 
@@ -21,12 +22,13 @@ def hill_estimator(df: pd.DataFrame,column: str, k_n: int):
     n=len(data_sorted)
 
     xi_hill=0
-    for i in range(n-k_n+1,n):
-        xi_hill+=np.log(data_sorted[i]/data_sorted[n-k_n+1])
+    for i in range(n-k_n,n):
+        xi_hill+=np.log(data_sorted[i]/data_sorted[n-k_n])
 
     return xi_hill
 
 def main():
+    # Load data
     data = pd.read_csv('natixis_stock.csv', delimiter='\t', names=['Date', 'Price'])
     data['Date'] = pd.to_datetime(data['Date'], format='%d/%m/%Y')
     data['Price'] = data['Price'].str.replace(',', '.').astype(float)
@@ -34,10 +36,16 @@ def main():
     data = data.dropna()
     print(data,"\n")
     print(data.shape,"\n")
-    print(data.info(),"\n")
 
+    # Plot data
+    plt.plot(data['Price'])
+    plt.show()
+
+    # Select the data
+    data = data[data['Return'] < 0]
     data['Return'] = abs(data['Return'])
 
+    # Compute the pickands and hill estimator
     k = 6
     xi_estimate = pickands_estimator(data,'Return', k)
     print(f"Pickands' estimate of ξ: {xi_estimate:.4f}")
@@ -46,4 +54,6 @@ def main():
     print(f"Hill's estimate of ξ: {xi_estimate:.4f}")
 
 if __name__ == "__main__":
+    print("----------------------------TD3-------------------------------")
     main()
+    print("--------------------------------------------------------------")
